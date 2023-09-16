c619f4026b436d68507e2d2cefab1edcdef8b4b6
Rivan Bello <rivanbello@gmail.com>
2023-09-16 20:48:48 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Gata, Gatona, Gatinha, se você me ama da uma risadinha!</h1>
+  return <h1>Gata, Gatona, Gatinha, se você me ama da uma risadinha!</h1>;
 }
 
 export default Home;
